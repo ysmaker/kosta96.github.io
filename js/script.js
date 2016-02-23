d746b2cc73f7a5f5e8632149b208db55eb3633ee
Kosta96 <khetagurov235@gmail.com>
2016-02-23 21:49:44 +0300

    clear
    clearall

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
 function addListItem() {
 	var text = $('#new-todo').val();
-	
 	if (text==="")
 	{ 
 		confirm("Write what u are going to do") ;
 	} 
 	else 
 	{
-		$('ul').append('<li><input type=checkbox><label>' + text + '</label></li>');
+		$('ul').append('<li><input type=checkbox><label contenteditable="true">' + text + '</label></li>');
+
 		$('#new-todo').val('');
 	}
 };
 
-//function done() {
-//if checkbox.checked
-//}
-
-//function () {
-//	$('#done').on('click', done);
-//}
+function clearAll() {
+	$('li').remove();
+}
 
 $(document).ready(function () {
     $('#add').on('click', addListItem);
+    $("#clearAll").on('click', clearAll);
  });
+
+
