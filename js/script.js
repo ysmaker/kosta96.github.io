3c4a4cd958f2a4b3e12cb3847c69f3ef111cc60b
Kosta96 <khetagurov235@gmail.com>
2016-03-06 19:15:57 +0300

    Added files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var countLi = $('ul.list li').size();
-
 function addListItem() {
+//	if(typeof(Storage) !== "undefined") {
 	var text = $('#new-todo').val();
 	if (text==="")
 	{ 
@@ -8,12 +8,22 @@ function addListItem() {
 	} 
 	else 
 	{
-		
-		$('ul').append('<li class='+ countLi +'><input class='+ countLi +' type=checkbox><label contenteditable="true">' + text + '</label></li>');
+		$('ul').prepend('<li class='+ countLi +'><input class='+ countLi +' type=checkbox><label class="todos" contenteditable="true">' + text + '</label></li>');
 		$('#new-todo').val('');
 		countLi+=1;
 	}
+		var todos = $('.todos').html();
+   		localStorage.setItem('todos', todos);
+   		return false;
+	
+	/* var todos = $('.lal').html();
+        localStorage.setItem('lal', todos);
+	} else {
+    // No Web Storage support..
+	}*/
 };
+
+
 
 function clearAll() {
 	$('li').remove();
@@ -34,27 +44,31 @@ function clearChecked() {
 }
 
 function selectAll() {
-$(':checkbox').attr('checked',true);
+	$(':checkbox').attr('checked',true);
 }
 
 $(document).ready(function () {
 	
-	 $('input').on('keypress',(function (e) {
+	$('input').on('keypress',(function (e) {
                 if (e.which == 13) {
                     addListItem();
                 }
             })
      );
-
     $('#add').on('click', addListItem);
-    
     $("#clearAll").on('click', clearAll);
- 	
  	$("#clearChecked").on('click',clearChecked);
- 	
 	$("#selectAll").on('click',selectAll);
-    });
 
+
+	/*if (localStorage.getItem('todos')) {
+        $('#todos').html(localStorage.getItem('todos'));
+    }*/
+});
+
+if(localStorage.getItem('todos')) {
+	$('li').html(localStorage.getItem('todos'));
+	}	
 
 
 
